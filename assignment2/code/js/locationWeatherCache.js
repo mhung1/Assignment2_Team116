4bd785de69a04630be86c3fb588c03773688d858
mhung1 <mhung1@student.monash.edu>
2016-05-17 00:49:00 +1000

    locationWeatherCache update
    Added serveral public methods

--- assignment2/code/js/locationWeatherCache.js
+++ assignment2/code/js/locationWeatherCache.js
@@ -38,12 +38,14 @@ function LocationWeatherCache()
     // Returns the number of locations stored in the cache.
     //
     this.length = function() {
+        return locations.length;
     };
     
     // Returns the location object for a given index.
     // Indexes begin at zero.
     //
     this.locationAtIndex = function(index) {
+        return locations[index];
     };
 
     // Given a latitude, longitude and nickname, this method saves a 
@@ -52,12 +54,20 @@ function LocationWeatherCache()
     //
     this.addLocation = function(latitude, longitude, nickname)
     {
+        var newLocation = {
+            latitube: latitude,
+            longitude: longitude,
+            nickname: nickname
+        }
+        
+        locations.push(newLocation);
     }
 
     // Removes the saved location at the given index.
     // 
     this.removeLocationAtIndex = function(index)
     {
+        location.splice[index, 1];
     }
 
     // This method is used by JSON.stringify() to serialise this class.
@@ -65,6 +75,7 @@ function LocationWeatherCache()
     // are active web service requests and so doesn't need to be saved.
     //
     this.toJSON = function() {
+        
     };
 
     // Given a public-data-only version of the class (such as from
