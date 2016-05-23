8f0ec19d547d6b0ecb21dc5be6f68ddf2bf4c653
stodl1 <Sophia@dyn-118-138-197-181.its.monash.edu.au>
2016-05-23 23:18:40 +1000

    Main Page
    This is just a bit - will fix and finish

--- assignment2/code/js/mainPage.js
+++ assignment2/code/js/mainPage.js
@@ -3,10 +3,31 @@
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
 
-function viewLocation(locationName)
-{
+function viewLocation(locationName) {
     // Save the desired location to local storage
-    localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName); 
+    localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName);
     // And load the view location page.
     location.href = 'viewlocation.html';
 }
+
+for (index = 0; index <= LocationWeatherCache.length; index++) {
+    loadLocations();
+    var outputAreaRef = document.getElementById("locationsListRef")
+    var output;
+    
+    output += locationWeatherCache.locationAtIndex(index)
+    
+    outputAreaRef.innerHTML = output
+    
+    
+}
+
+ /*<li class="mdl-list__item mdl-list__item--two-line" onclick="viewLocation(0);">
+                <span class="mdl-list__item-primary-content">
+                  <img class="mdl-list__item-icon" id="icon0" src="images/loading.png" class="list-avatar" />
+                  <span>Location A</span>
+                  <span id="weather0" class="mdl-list__item-sub-title">Weather summary</span>
+                </span>
+              </li>
+              
+*/
