dcd5b1ea12c3ee4bff3762b5c62a576b0b349b9d
avishkam <avishka-m@hotmail.com>
2016-05-24 16:58:57 +1000

    Add files via upload

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -11,15 +11,36 @@ var addLocationButtonRef = document.getElementById("addlocationButton");
 //
 function initialize() 
 {
-    var mapProp = 
-        {
-            center:new google.maps.LatLng(-37.908042,145.1325757),
-            zoom:15,
-            mapTypeId:google.maps.MapTypeId.ROADMAP
-        };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  var map=new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+  });
+  var geocoder=new google.maps.Geocoder();
+   document.getElementById('submit')addEventListener('click',function() {
+    geocodeAdress(geocoder, map);  
+   });
 }
-//google.maps.event.addDomListener(window, 'load', initialize);
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      
+        lat=results[0].geometry.loacation.lat();
+        lng=results[0].geometry.location.lng();
+        
+        addressRef.value = results[0].formatted_address;
+    
+        
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
 
 
 // search the location
@@ -31,13 +52,7 @@ function search()
     document.body.appendChild(script);
 }
 
-// callback function
-//
-function geoResponse (response)
-{
-   lat = response.geometry.location.lat;
-   lng = response.geometry.location.lng;
-}
+
 
 
 // execute when addlocation button is clicked
