eb610f989e3863ef66489b35fd02471d63f7e75a
stodl1 <Sophia@dyn-118-138-197-181.its.monash.edu.au>
2016-05-25 01:14:46 +1000

    More work on main page

--- assignment2/code/js/mainPage.js
+++ assignment2/code/js/mainPage.js
@@ -3,31 +3,27 @@
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
 
-function viewLocation(locationName) {
+function viewLocation(locationIndex) {
     // Save the desired location to local storage
     localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName);
     // And load the view location page.
     location.href = 'viewlocation.html';
 }
+loadLocations();
+var today = new Date()
 
-for (index = 0; index <= LocationWeatherCache.length; index++) {
-    loadLocations();
+for (var index = 0; index <= LocationWeatherCache.length; index++) {
+    
     var outputAreaRef = document.getElementById("locationsListRef")
     var output;
     
+    
     output += locationWeatherCache.locationAtIndex(index)
+    output += '<li class="mdl-list__item mdl-list__item--two-line" onclick="viewLocation('index');"><span class="mdl-list__item-primary-content"><img class="mdl-list__item-icon" id="'locationWeatherCache.weatherResponse.weather.icon(index)'" src="images/loading.png" class="list-avatar" /><span>'locationWeatherCache.locations.nickname(index)'</span><span id="weather'index'" class="mdl-list__item-sub-title">'this.getWeatherAtIndexForDate(index, today, callback)'</span></span></li>'
     
     outputAreaRef.innerHTML = output
     
     
-}
+};
 
- /*<li class="mdl-list__item mdl-list__item--two-line" onclick="viewLocation(0);">
-                <span class="mdl-list__item-primary-content">
-                  <img class="mdl-list__item-icon" id="icon0" src="images/loading.png" class="list-avatar" />
-                  <span>Location A</span>
-                  <span id="weather0" class="mdl-list__item-sub-title">Weather summary</span>
-                </span>
-              </li>
-              
-*/
+
