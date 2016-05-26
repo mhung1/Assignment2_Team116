d7714f9023b4904a862a2697369afcb815e8277d
mhung1 <mhung1@student.monash.edu>
2016-05-26 12:33:46 +1000

    fixed map stuffs

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -2,62 +2,87 @@
 loadLocations();
 
 // DOM References:
-var locationInputRef = document.getElementById("locationInput");
-var nickNameInputRef = document.getElementById("nickNameInput");
+var addressRef = document.getElementById("address");
+var nicknameRef = document.getElementById("nickname");
 var searchButtonRef = document.getElementById("searchButton");
-var addLocationButtonRef = document.getElementById("addlocationButton");
+var addButtonRef = document.getElementById("addButton");
 
-// map initialize
-//
-function initialize() 
-{
-  var map=new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-  });
-  var geocoder=new google.maps.Geocoder();
-   document.getElementById('submit')addEventListener('click',function() {
-    geocodeAdress(geocoder, map);  
-   });
-}
-
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('address').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      resultsMap.setCenter(results[0].geometry.location);
-      
-        lat=results[0].geometry.loacation.lat();
-        lng=results[0].geometry.location.lng();
-        
-        addressRef.value = results[0].formatted_address;
-    
-        
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+// Attributes:
+var latitude;
+var longitude;
 
 
-// search the location
+// search button onclick function that geocode the address
 //
 function search()
 {
-    var script = document.createElement('script');
-    script.src = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locationInputRef.value +"&key=AIzaSyAlmgZ1Nj0KIWIRSEKsQVHFFwAW78hy1G4&callback=this.geoResponse";
-    document.body.appendChild(script);
+    geocodeAddress(geocoder, map);
 }
 
 
+// map initialization
+//
+function initMap() 
+{
+    // Create a map
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+    });
+    
+    infowindow = new google.maps.InfoWindow;
+    geocoder = new google.maps.Geocoder();
+}
+
+// A reference code of geocode example on https://developers.google.com/
+// maps/documentation/javascript/examples/geocoding-simple
+// since Google's geocoding API doesn't support JSONP
+//
+function geocodeAddress(geocoder, resultsMap) {
+    var addressObj = {
+        'address': addressRef.value
+    };
+    geocoder.geocode(addressObj, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            // set the centre of the map at specified location
+            resultsMap.setCenter(results[0].geometry.location);
+            
+            // get latitude and longitude values
+            latitude = results[0].geometry.location.lat();
+            longitude = results[0].geometry.location.lng();
+            
+            // create a marker and place it at the centre
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+            
+            // show the formatted address
+            infowindow.setContent(results[0].formatted_address);
+            infowindow.open(map, marker);
+        }
+        else
+        {
+        alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
 
 
 // execute when addlocation button is clicked
 function addLocation ()
 {
-    locationWeatherCache.addLocation(lat, lng, nickNameInputRef.value);
-    location.href = 'index.html'; // go back to main page
+    // using if statement to check: 1. nickname, 2. location
+    // hints: nicknameRef = ""
+    //        latitude and longitude is undefined or not
+    
+    // if ([something])
+    // {
+            locationWeatherCache.addLocation(lat, lng, nickNameInputRef.value);
+            location.href = 'index.html'; // go back to main page
+    // }
+    // else
+    // {
+    //    something goes wrong, alert the user to search location or type in Nickname
+    //    hints: use alert([message you want to display])
+    // }
 }
